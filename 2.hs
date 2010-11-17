cfdd4cd671c342c92dec75f59db943a645e7248d
Tianyi Cui <tianyicui@gmail.com>
2010-11-17 14:47:55 +0800

    2' 2''

--- 2.hs
+++ 2.hs
@@ -1,2 +1,8 @@
 myButLast [x,_]  = x
 myButLast (_:xs) = myButLast xs
+
+-- Why can't `!!` support negative number like Python/Ruby do?
+myButLast' x = reverse x !! 1
+
+-- First time to know `.`
+myButLast'' = last . init

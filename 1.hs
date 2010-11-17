243a394a8cefc19a8ed287bab77d5f030ad19ceb
Tianyi Cui <tianyicui@gmail.com>
2010-11-17 14:49:40 +0800

    ---- to split my own solutions and others

--- 1.hs
+++ 1.hs
@@ -1,5 +1,7 @@
 myLast [x]    = x
 myLast (_:xs) = myLast xs
 
+----
+
 -- XXX: Did not quite get it
 myLast' = foldr1 (const id)
--- 2.hs
+++ 2.hs
@@ -1,6 +1,8 @@
 myButLast [x,_]  = x
 myButLast (_:xs) = myButLast xs
 
+----
+
 -- Why can't `!!` support negative number like Python/Ruby do?
 myButLast' x = reverse x !! 1
 

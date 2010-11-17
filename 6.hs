82e9d5c3b50446a953969f736b1592e6065eec14
Tianyi Cui <tianyicui@gmail.com>
2010-11-17 15:32:35 +0800

    6 revise

--- 6.hs
+++ 6.hs
@@ -1,5 +1,5 @@
 isPalindrome xs = (xs == (reverse xs))
 
 isPalindrome' []  = True
-isPalindrome' [x] = True
+isPalindrome' [_] = True
 isPalindrome' xs  = (head xs) == (last xs) && (isPalindrome' $ init $ tail xs)

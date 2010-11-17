c400da95343ad41cf35a77118f8b7d3e903d0689
Tianyi Cui <tianyicui@gmail.com>
2010-11-17 18:22:07 +0800

    9'

--- 9.hs
+++ 9.hs
@@ -6,3 +6,11 @@ pack (x:xs) =
         | otherwise                 = ([x]++c, d)
             where (c, d) = dropAndConcat x (tail xs)
 pack [] = []
+
+pack' :: (Eq a) => [a] -> [[a]]
+pack' = foldr func []
+    where
+    func x []     = [[x]]
+    func x (y:xs)
+        | null y || x /= (head y) = ([x]:y:xs)
+        | otherwise               = ((x:y):xs)

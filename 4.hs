a49ed3f67ad6bc4c92d4f69031dd9f0a51094080
Tianyi Cui <tianyicui@gmail.com>
2010-11-17 15:24:04 +0800

    4'' & 4'''

--- 4.hs
+++ 4.hs
@@ -3,3 +3,8 @@ myLength (_:xs) = 1 + (myLength xs)
 
 myLength' xs = foldl addOne 0 xs where
   addOne x _ = 1 + x
+
+---- Learned lambuda, and the xs parameter is not necessary
+
+myLength''  = foldr (\_ n -> n + 1) 0
+myLength''' = foldl (\n _ -> n + 1) 0

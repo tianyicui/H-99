46c7fb388e9e5e310ecfb1c8ac0a2571d2bf7f28
Tianyi Cui <tianyicui@gmail.com>
2010-11-17 18:02:32 +0800

    8'

--- 8.hs
+++ 8.hs
@@ -2,3 +2,11 @@ compress (x:y:xs)
     | x == y    = compress (y:xs)
     | otherwise = x:(compress (y:xs))
 compress x      = x
+
+compress' :: (Eq a) => [a] -> [a]
+compress' = foldr func []
+    where
+    func x (y:xs)
+        | x == y    = (y:xs)
+        | otherwise = (x:y:xs)
+    func x []       = [x]

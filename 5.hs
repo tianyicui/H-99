d7ee834fd602d665b19b9dd9aafbd6821f44efc3
Tianyi Cui <tianyicui@gmail.com>
2010-11-17 15:29:32 +0800

    5'' && 5'''

--- 5.hs
+++ 5.hs
@@ -3,3 +3,12 @@ myReverse (x:xs) = (myReverse xs) ++ [x]
 
 myReverse' xs = foldl func [] xs where
     func xs x = x:xs
+
+----
+
+-- the concise standard version
+myReverse'' = foldl (flip (:)) []
+
+myReverse''' = doReverse [] where
+    doReverse reversed []     = reversed
+    doReverse reversed (x:xs) = doReverse (x:reversed) xs

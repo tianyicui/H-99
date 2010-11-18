52cc931dac37da3da0977315f2d4cab16c95a1db
Tianyi Cui <tianyicui@gmail.com>
2010-11-18 11:59:33 +0800

    14' 14'' 14'''

--- 14.hs
+++ 14.hs
@@ -1,1 +1,14 @@
 dupli = concatMap (\x -> [x,x])
+
+----
+
+-- use list comprehension
+dupli' xs = concat [[x,x] | x <- xs]
+
+-- the direct solution
+dupli'' [] = []
+dupli'' (x:xs) = x:x:dupli' xs
+
+-- the direct solution using foldr
+dupli''' = foldr (\x xs -> x:x:xs) []
+

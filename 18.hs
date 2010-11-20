cbcd0e40f994f862b2157066a1ed431a8a1be443
Tianyi Cui <tianyicui@gmail.com>
2010-11-20 14:24:01 +0800

    18''

--- 18.hs
+++ 18.hs
@@ -1,5 +1,9 @@
 slice xs i j = take (j-i+1) (drop (i-1) xs)
 
+slice'' xs i j = map snd
+               $ filter ( (\x -> x >= i && x <= j) . fst )
+               $ zip [1..] xs
+
 ---- so I can also write like this
 ---- XXX: works with ghc 6.12.3 but not 7.0.1!
-slice' xs (i+1) j = take (j-i) $ drop i xs
+-- slice' xs (i+1) j = take (j-i) $ drop i xs

5c8d471635a57050b645f44108c8d502c815ee58
Tianyi Cui <tianyicui@gmail.com>
2010-11-20 13:44:59 +0800

    16'

--- 16.hs
+++ 16.hs
@@ -1,2 +1,5 @@
 dropEvery [] _ = []
 dropEvery xs n = (take (n-1) xs) ++ dropEvery (drop n xs) n
+
+---- using zip, seems like sth to avoid
+dropEvery' xs n = map snd . filter ((n/=) . fst) . zip (cycle [1..n]) $ xs

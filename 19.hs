97528a341b6112928b5f69d29b8980822f42e9f0
Tianyi Cui <tianyicui@gmail.com>
2010-11-20 13:31:51 +0800

    19'

--- 19.hs
+++ 19.hs
@@ -2,3 +2,9 @@ rotate xs n
     | null xs || n == 0 = xs
     | n > 0 = rotate ( (tail xs)++[head xs] ) ( n - 1 )
     | n < 0 = rotate ( (last xs): (init xs) ) ( n + 1 )
+
+rotate' xs n
+    | n >= 0 =
+        let (fst, snd) = splitAt n xs
+        in snd ++ fst
+    | otherwise = rotate xs (n `mod` (length xs))

be3fd6d82d36276d20cfc270ce20522bfde7e9b7
Tianyi Cui <tianyicui@gmail.com>
2010-11-23 17:38:02 +0800

    28(a)' & 28(b)

--- 28.hs
+++ 28.hs
@@ -1,3 +1,15 @@
-import Data.List (sortBy)
+import Data.List (sortBy, groupBy)
+import Data.Ord (comparing)
 
 lsort = sortBy (\x y -> compare (length x) (length y))
+
+----
+
+-- this looks better
+lsort' = sortBy (comparing length)
+
+-- this solution is brilliant.
+-- it's hard to write a O(NlogN) solution using only List processing.
+lfsort lists = concat groups
+    where groups = lsort $ groupBy equalLength $ lsort lists
+          equalLength xs ys = length xs == length ys

22827f42a22e1b55f558cb867f902b3eb46b0f7f
Tianyi Cui <tianyicui@gmail.com>
2010-11-25 15:01:36 +0800

    27'

--- 27.hs
+++ 27.hs
@@ -1,4 +1,4 @@
-group []     [] = [[]]
+group []     _  = [[]]
 group (n:ns) xs =
     [ g:gs | (g,rs) <- comb n xs
            ,  gs    <- group ns rs ]
@@ -9,3 +9,8 @@ comb n (x:xs) =
     [ (x:h,t) | (h,t) <- comb (n-1) xs ]
     [ (h,x:t) | (h,t) <- comb  n    xs ]
 comb _ _      = []
+
+---- TODO: another implementation which is hard to understand
+group' :: [Int] -> [a] -> [[[a]]]
+group' [] = const [[]]
+group' (n:ns) = concatMap (uncurry $ (. group' ns) . map . (:)) . comb n

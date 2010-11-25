30e04c4cd09d83f8270760ace78f0e248e0cc3f4
Tianyi Cui <tianyicui@gmail.com>
2010-11-25 14:35:00 +0800

    25'

--- 25.hs
+++ 25.hs
@@ -7,3 +7,12 @@ rnd_permu (x:xs) = do
     rest <- rnd_permu xs
     return $ let (ys,zs) = splitAt rand rest
              in ys++(x:zs)
+
+rnd_permu' [] = return []
+rnd_permu' xs = do
+    rand <- randomRIO (0, (length xs)-1)
+    rest <- let (ys,(_:zs)) = splitAt rand xs
+            in rnd_permu' $ ys ++ zs
+    return $ (xs!!rand):rest
+
+-- TODO: an O(N) algorithm

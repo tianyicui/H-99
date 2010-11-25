74c77cf180923b08c817161d4a20acaaaf66cdc2
Tianyi Cui <tianyicui@gmail.com>
2010-11-25 14:43:26 +0800

    26'

--- 26.hs
+++ 26.hs
@@ -1,5 +1,12 @@
+import Data.List(tails)
+
 combinations 0 _      = [[]]
 combinations _ []     = []
 combinations n (x:xs) =
     (map (x:) $ combinations (n-1) xs)
     ++ combinations n xs
+
+---- a solution using tails and list comprehension
+combinations' 0 _  = [ [] ]
+combinations' n xs = [ y:ys | y:xs' <- tails xs
+                            , ys <- combinations' (n-1) xs']

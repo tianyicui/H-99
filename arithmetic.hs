6d9af7a5198a2e53ff223b2b9c146525e836fbfa
Tianyi Cui <tianyicui@gmail.com>
2010-11-25 16:42:41 +0800

    33 & 33'

--- arithmetic.hs
+++ arithmetic.hs
@@ -9,3 +9,15 @@ isPrime n = and $ [ n `mod` x /= 0 | x <- takeWhile (\x -> x*x <= n) [2..] ]
 --------
 
 myGCD x y = if x == 0 then y else myGCD (y `mod` x) x
+
+--------
+-- 33 --
+--------
+
+coprime x y
+    | x >  y    = coprime y x
+    | x == 0    = False
+    | x == 1    = True
+    | otherwise = coprime (y `mod` x) x
+
+coprime' x y = (myGCD x y) == 1

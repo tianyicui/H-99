28c416536bb6660b64edb92ac2c47b869880e8f3
Tianyi Cui <tianyicui@gmail.com>
2010-11-25 14:41:16 +0800

    26 revised

--- 26.hs
+++ 26.hs
@@ -1,5 +1,5 @@
-combinations 1 xs     = map (\x -> [x]) xs
+combinations 0 _      = [[]]
+combinations _ []     = []
 combinations n (x:xs) =
     (map (x:) $ combinations (n-1) xs)
-    ++ (combinations n xs)
-combinations _ _      = []
+    ++ combinations n xs

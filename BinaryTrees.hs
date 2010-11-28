50474063f39196c9909904f9681c601b401f4a9e
Tianyi Cui <tianyicui@gmail.com>
2010-11-28 06:10:01 +0000

    55

--- BinaryTrees.hs
+++ BinaryTrees.hs
@@ -4,3 +4,14 @@ data Tree a = Empty | Branch a (Tree a) (Tree a)
 isTree x = case x of
     Empty        -> True
     Branch _ _ _ -> True
+
+cbalTree 0 = [Empty]
+cbalTree n =
+    [ Branch 'x' ta tb
+    | let p0 = (n-1) `div` 2
+    , let p1 =  n    `div` 2
+    , p <- [p0..p1]
+    , let q = n - 1 - p
+    , ta <- cbalTree p
+    , tb <- cbalTree q
+    ]
